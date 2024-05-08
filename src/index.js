24537d35c6804aa220afcdcadf796d215a498395
RAKESHRD2 <rakeshreddy.dodda@slu.com>
2024-05-07 22:18:43 -0500

    id_card_img

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
+const csv = require('csv-parser');
+const path = require('path');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Function to create ID cards
+function createIDCards(templateImagePath, csvFilePath, photoDirectory, outputFilePath) {
+    const doc = new PDFDocument({ autoFirstPage: false });
+    const templateImage = path.join(__dirname, templateImagePath);
+    const photoDir = path.join(__dirname, photoDirectory);
+    const outputStream = fs.createWriteStream(outputFilePath);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // Pipe the PDF to the output file
+    doc.pipe(outputStream);
+
+    // Read employee data from CSV
+    fs.createReadStream(csvFilePath)
+        .pipe(csv())
+        .on('data', (data) => {
+            // Start a new page for each employee
+            doc.addPage({ size: [300, 480] }); // Standard ID card size
+
+            // Draw template image
+            doc.image(templateImage, 0, 0, { width: 300 });
+
+            // Load employee photo
+            const photoPath = path.join(photoDir, data.photo);
+            doc.image(photoPath, 50, 120, { width: 200 });
+
+            // Add employee details
+            doc.font('Helvetica').fontSize(12);
+            doc.text(data.name, 50, 50);
+            doc.text(data.title, 50, 70);
+        })
+        .on('end', () => {
+            // Finalize the PDF
+            doc.end();
+            console.log('PDF created successfully!');
+        });
+}
+
+// Usage: createIDCards(templateImagePath, csvFilePath, photoDirectory, outputFilePath)
+createIDCards('ute_id_template.png', 'employees.csv', 'photos', 'id_cards.pdf');
